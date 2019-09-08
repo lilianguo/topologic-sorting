333f470f269a340a6659a95b581a91a74b523e00
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-08 16:39:00 -0700

    Update CourseScheduleII.java

--- CourseScheduleII.java
+++ CourseScheduleII.java
@@ -1,51 +1,64 @@
 class CourseScheduleII {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List[] edges = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
-        //下一个点的可能的点由arraylist保存
+        Map<Integer, Set<Integer>> graph = createGrapth(prerequisites);
+        return topologicSort(numCourses, graph);
+    }
+    private Map<Integer, Set<Integer>> createGrapth (int[][] prerequisites) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int[] prerequisite : prerequisites) {
+            int pre = prerequisite[1];
+            int curr = prerequisite[0];
+            System.out.println("pre is :" + pre);
+            System.out.println("curr is :" + curr);
+            graph.putIfAbsent(pre, new HashSet<>());
+            graph.get(pre).add(curr);
+        }
+        System.out.println("grapth size is :" + graph.size());
+        return graph;
+    }
+
+    private Map<Integer, Integer> getIndegree(int numCourses, Map<Integer, Set<Integer>> graph) {
+        Map<Integer, Integer> indegree = new HashMap<>();
         for (int i = 0; i < numCourses; i++) {
-            edges[i] = new ArrayList<>();
+            indegree.put(i, 0);
         }
-        // degree保存每个点有多少个上一个点， 如果没有，这个点可以作为开头
-        // edges保存每个点对应的下一个点都是什么
-        for (int i = 0; i < prerequisites.length; i++) {
-            degree[prerequisites[i][0]]++;
-            edges[prerequisites[i][1]].add(prerequisites[i][0]);
-        }
-        // queue中开始压入没有上一个点的点
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i=  0; i < degree.length; i++) {
-            if (degree[i] == 0) {
-                queue.offer(i);
+        for (int graphKey : graph.keySet()) {
+            for (int next : graph.get(graphKey)) {
+                indegree.put(next, indegree.get(next) + 1);
             }
         }
-        int count = 0;
-        List<Integer> res = new ArrayList<>();
+        return indegree;
+    }
+    private int[] topologicSort(int numCourses, Map<Integer, Set<Integer>> graph) {
+        Map<Integer, Integer> indegree = getIndegree(numCourses, graph);
+        Queue<Integer> queue = new LinkedList<>();
+        for (int key : indegree.keySet()) {
+            if (indegree.get(key) == 0) {
+                System.out.println("head : " + key);
+                queue.add(key);
+            }
+        }
+        List<Integer> courses = new ArrayList<>();
         while(!queue.isEmpty()) {
-            int course = queue.poll();
-            res.add(course);
-            count++;
-            // 取出这个头部有几个下一个点
-            int n = edges[course].size();
-            for (int i = 0; i < n; i++) {
-                int next = (int)edges[course].get(i);
-                //拿出的这个点，它的 # of上一个点就会减少一个
-                degree[next]--;
-                // 如果前面没有电了，说明可以压入queue
-                if (degree[next] == 0) {
-                    queue.add(next);
+            int curr = queue.poll();
+            courses.add(curr);
+            if (graph.get(curr) != null) {
+                for (int next : graph.get(curr)) {
+                    indegree.put(next, indegree.get(next) - 1);
+                    if (indegree.get(next) == 0) {
+                        queue.offer(next);
+                    }
                 }
             }
         }
-        if (count == numCourses) {
-            int[] schedule = new int[numCourses];
+        if (courses.size() == numCourses) {
+            int[] res = new int[numCourses];
             for (int i = 0; i < numCourses; i++) {
-                schedule[i] = res.get(i);
+                res[i] = courses.get(i);
             }
-            return schedule;
-        } else {
-            return new int[0];
+            return res;
         }
-        // return count == numCourses ? res.toArray() : new int[0];
+        return new int[0];
     }
+        // return count == numCourses ? res.toArray() : new int[0];
 }
