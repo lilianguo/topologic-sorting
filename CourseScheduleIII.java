c5bf56ff44754995eb7f0e049b8719b03b06b6e1
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-09-08 17:33:03 -0700

    Update CourseScheduleIII.java

--- CourseScheduleIII.java
+++ CourseScheduleIII.java
@@ -1,5 +1,29 @@
 class CourseScheduleIII {
+    /*
+    Time complexity : O(nlog(n). At most n elements are added to the queue. 
+    Adding each element is followed by heapification, which takes O(log(n) time.
+
+    Space complexity : O(n). 
+    The queuequeue containing the durations of the courses taken can have atmost n elements
+    */
+
     public int scheduleCourse(int[][] courses) {
-        
+        if (courses == null || courses.length == 0) {
+            return 0;
+        }
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        int time = 0;
+        for (int i = 0; i < courses.length; i++) {
+            if (time + courses[i][0] <= courses[i][1]) {
+                time += courses[i][0];
+                queue.offer(courses[i][0]);
+            }
+            else if (!queue.isEmpty() && courses[i][0] < queue.peek()) {
+                time += courses[i][0] - queue.poll();
+                queue.offer(courses[i][0]);
+            }
+        }
+        return queue.size();
     }
 }
