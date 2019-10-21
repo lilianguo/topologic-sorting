3cf15ab2e7f522908b7f1d46b8ec3b87ca4e4085
Rebecca Guo <rebeccaguospecial@gmail.com>
2019-10-20 17:55:57 -0700

    Update CourseSchedule.java

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -1,61 +1,82 @@
-import java.util.Map;
+/*
+207. Course Schedule
+There are a total of n courses you have to take, labeled from 0 to n-1.
 
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: 2, [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+
+*/
 class CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, Set<Integer>> graph = createGrapth(prerequisites);
+        Map<Integer, Set<Integer>> graph = createGraph(numCourses, prerequisites);
         return topologicSort(numCourses, graph);
     }
-    private Map<Integer, Set<Integer>> createGrapth (int[][] prerequisites) {
+    private Map<Integer, Set<Integer>> createGraph(int numCourses, int[][] prerequisites) {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.putIfAbsent(i, new HashSet<>());
+        }
         for (int[] prerequisite : prerequisites) {
             int pre = prerequisite[1];
             int curr = prerequisite[0];
-            System.out.println("pre is :" + pre);
-            System.out.println("curr is :" + curr);
-            graph.putIfAbsent(pre, new HashSet<>());
             graph.get(pre).add(curr);
         }
-        System.out.println("grapth size is :" + graph.size());
         return graph;
     }
 
-    private Map<Integer, Integer> getIndegree(int numCourses, Map<Integer, Set<Integer>> graph) {
+    private boolean topologicSort(int numCourses, Map<Integer, Set<Integer>> graph) {
         Map<Integer, Integer> indegree = new HashMap<>();
         for (int i = 0; i < numCourses; i++) {
-            indegree.put(i, 0);
+            indegree.putIfAbsent(i, 0);
         }
-        for (int graphKey : graph.keySet()) {
-            for (int next : graph.get(graphKey)) {
+        for (int key : graph.keySet()) {
+            for (int next : graph.get(key)) {
                 indegree.put(next, indegree.get(next) + 1);
             }
-        }
-        return indegree;
-    }
-    private boolean topologicSort(int numCourses, Map<Integer, Set<Integer>> graph) {
-        Map<Integer, Integer> indegree = getIndegree(numCourses, graph);
+        } 
+
         Queue<Integer> queue = new LinkedList<>();
-        for (int key : indegree.keySet()) {
+        for (int key : indegree.keySet()){
             if (indegree.get(key) == 0) {
-                System.out.println("head : " + key);
-                queue.add(key);
+                queue.offer(key);
             }
         }
+
+        if (queue.size() == numCourses) {
+            return true;
+        }
+
         List<Integer> courses = new ArrayList<>();
-        while(!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int curr = queue.poll();
+            System.out.println("curr is " + curr);
             courses.add(curr);
-            if (graph.get(curr) != null) {
-                for (int next : graph.get(curr)) {
-                    indegree.put(next, indegree.get(next) - 1);
-                    if (indegree.get(next) == 0) {
-                        queue.offer(next);
-                    }
+            for (int next : graph.get(curr)) {
+                indegree.put(next, indegree.get(next) - 1);
+                if (indegree.get(next) == 0) {
+                    queue.offer(next);
                 }
             }
         }
-        if (courses.size() == numCourses) {
-            return true;
-        }
-        return false;
+        return courses.size() == numCourses;
     }
 }
